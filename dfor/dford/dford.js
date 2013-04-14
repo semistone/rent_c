d3f49606e67bf6b23c96df9a8caa83c7d43e4667
Ubuntu <ubuntu@ip-10-142-55-159.ap-southeast-1.compute.internal>
2013-04-14 06:32:35 +0000

    check socket

--- dfor/dford/dford.js
+++ dfor/dford/dford.js
@@ -5,6 +5,7 @@
  *
  */
 var _ = require('underscore')._;
+var net = require('net');
 var yaml = require("js-yaml"), confpath = process.argv[2];
 //
 //  open sqlite
@@ -19,8 +20,61 @@ var db = new sqlite3.Database(dbfile);
 var conffile = confpath + "/config.yaml";
 console.log('config file in ' + conffile);
 var config = require(conffile);
+var callback = {
+    error: function(name, ip, last_status){
+        if(last_status.status == 1) {
+            last_status.status = 0;
+            var ts = Math.round(Date.now() / 1000);
+            console.log('status change to 0');
+            db.run('update HOST set STATUS=0, MODIFIED=? where NAME=? and  IP=?', [ts, name, ip]);
+        }
+        console.log('connect error for name:' + name + ' ip:' + ip);
+    },
+    connect: function(name, ip, last_status){
+        console.log('connect error for name:' + name + ' ip:' + ip);
+        if(last_status.status == 0) {
+            last_status.status = 1;
+            var ts = Math.round(Date.now() / 1000);
+            console.log('status change to 1');
+            db.run('update HOST set STATUS=1, MODIFIED=? where NAME=? and  IP=?', [ts, name, ip]);
+        }
+    }
+};
 
-console.log(JSON.stringify(config, null, "    "));
+function check_socket(name, ip , port, callback, last_status) {//{{{
+    var socket = net.createConnection(port, ip);
+    socket.setTimeout(3000);
+    socket.on("error", function (err) {
+        console.log("socket error: " + err);
+        socket.end();
+        socket.destroy();
+        if(callback) callback.error(name, ip, last_status);
+    }).on('connect', function() {
+        console.log('socket connected');
+        socket.end();
+        socket.destroy();
+        if(callback) callback.connect(name, ip, last_status);
+    }).on('timeout', function(){
+        console.log("socket timeout: ");
+        socket.end();
+        socket.destroy();
+        if(callback) callback.error(name, ip, last_status);
+    });
+}//}}}
+
+
+function start(name, ip, settings){//{{{
+    var method = settings['check_method'], last_status = {'status' : 0};
+    setInterval((function() {
+        if(method == 'socket'){
+            var port = settings['check_port'];
+            console.log('check socket port:' + port);
+            check_socket(name, ip, port, callback, last_status);
+        }
+    }), 5000);
+}//}}}
+
+//console.log(JSON.stringify(config, null, "    "));
 //
 // init and insert  hosts into db
 //
@@ -41,27 +95,32 @@ for(var name in config){
         if(mode == 'weight'){
             weight = hosts[ip].weight;
         }
-        console.log('ip is ' + ip);
         var insert_stmt = db.prepare("insert or replace into HOST (NAME,IP,COUNT, WEIGHT,MODIFIED) values (?,?,0,?,?)");
         var ts = Math.round(Date.now() / 1000);
-        insert_stmt.run([name, ip, weight, ts]);
-        insert_stmt.finalize();
+        (function(){
+           var _name = name, _ip = ip, _settings = config[name];
+           insert_stmt.run([name, ip, weight, ts], function(){
+               start(_name, _ip, _settings);        
+           });
+           insert_stmt.finalize();
+        })();
     }
     //
     // delete extra record
     //
-    db.each("select name, ip from HOST where name = ? ", [name], function(err, row) {
-        console.log("query " + row.NAME + ":" + row.IP);
-        if(isarray){
-            if(_.indexOf(hosts, row.IP) == -1) {
-                console.log("1.name:" + row.NAME+ " ip:" + row.IP + " not exist in config");
-                db.run('delete from HOST where NAME=? and  IP=?', [row.NAME, row.IP]);
+    (function(name, _hosts, isarray){
+        db.each("select name, ip from HOST where name = ? ", [name], function(err, row) {
+            if(isarray){
+                if(_.indexOf(_hosts, row.IP) == -1) {
+                    console.log("1.name:" + row.NAME+ " ip:" + row.IP + " not exist in config");
+                    db.run('delete from HOST where NAME=? and  IP=?', [row.NAME, row.IP]);
+                }
+            } else {
+                if (_hosts[row.IP] == undefined) {
+                    console.log("2.name:" + row.NAME + " ip:" + row.IP + " not exist in config");
+                    db.run('delete from HOST where NAME=? and  IP=?', [row.NAME, row.IP]);
+                }
             }
-        } else {
-            if (hosts[row.IP] == undefined) {
-                console.log("2.name:" + row.NAME + " ip:" + row.IP + " not exist in config");
-                db.run('delete from HOST where NAME=? and  IP=?', [row.NAME, row.IP]);
-            }
-        }
-    });
+        });
+    })(name, hosts, isarray);
 }
