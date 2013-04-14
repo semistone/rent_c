c42760ce13929144c151fb0334a375a66064449d
Angus Chen <semistone@gmail.com>
2013-04-14 13:14:55 +0000

    check timeout

--- test_nodejs/hello_socket.js
+++ test_nodejs/hello_socket.js
@@ -1,10 +1,15 @@
-var port = 9090, host='127.0.0.1';
+var port = 9090, host='192.168.0.1';
 var net = require('net');
 var socket = net.createConnection(port, host,function(){
     console.log('Socket create ....');        
 });
+socket.setTimeout(3000);
 socket.on("error", function (err) {
      console.log("Socket error: " + err);
 }).on('connect', function() {
     console.log('Connected');
+}).on('timeout', function(){
+    console.log('Timeout');
+    socket.destroy();
 });
+console.log('gogogo');
