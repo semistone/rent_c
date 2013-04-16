49591549f9b2e0440816b2149872f901e4f30b77
Angus Chen <semistone@gmail.com>
2013-04-16 13:44:33 +0000

    put service into port

--- dfor/src/Hook.cc
+++ dfor/src/Hook.cc
@@ -39,6 +39,35 @@ init_dfor(){//{{{
     return 0;
 }//}}}
 
+/**
+ * for debug
+ */
+void dump_addr(addrinfo* res, const char* service){//{{{
+    printf("service is %s", service);
+    if (res == NULL) {
+        printf("addr is null\n");
+        return;
+    }
+    void *ptr;
+    char addrstr[100];
+    struct sockaddr_in *sock = (struct sockaddr_in *)res->ai_addr;
+    if (sock != NULL) {
+        printf("family is (%d) port (%d) zero(%s)\n", sock->sin_family, sock->sin_port, sock->sin_zero);
+    } else {
+        printf("sock is null\n");
+    }
+    ptr = &sock->sin_addr;
+    inet_ntop (res->ai_family, ptr, addrstr, 100);
+    printf ("IPv%d address: %s (%s) flag (%d) socket type (%d) protocol (%d) len (%d)\n", 
+            res->ai_family == PF_INET6 ? 6 : 4,
+            addrstr,
+            res->ai_canonname,
+            res->ai_flags,
+            res->ai_socktype,
+            res->ai_protocol,
+            res->ai_addrlen);
+}//}}}
+
 struct hostent *gethostbyname(const char *name){//{{{
     // init 
     if (db == NULL){
@@ -71,6 +100,8 @@ struct hostent *gethostbyname(const char *name){//{{{
 
 int getaddrinfo(const char *node, const char *service, const struct addrinfo *hints, struct addrinfo **res){//{{{
     // init 
+    //dump_addr(*res, service);
+    //dump_addr((struct addrinfo*)hints, service);
     if (db == NULL){
         if(init_dfor() != 0) return -1;
     }
@@ -88,23 +119,24 @@ int getaddrinfo(const char *node, const char *service, const struct addrinfo *hi
         logger->debugStream()<<"ip is not empty";
         struct sockaddr_in *sock;
         struct addrinfo  *addr;
-        char* name = (char*) malloc(100);
-        snprintf(name, 100, "%s", node); 
         addr = (addrinfo*) malloc(sizeof(addrinfo));
         sock = (sockaddr_in*) malloc(sizeof(sockaddr_in));
         memset(addr, 0, sizeof(struct addrinfo));
         memset(sock, 0, sizeof(struct sockaddr_in));
         addr->ai_family = AF_INET;
+        addr->ai_flags = hints->ai_flags;
         addr->ai_socktype = 1;
         addr->ai_addr = (sockaddr*)sock;
         addr->ai_addrlen = sizeof(sockaddr_in);
         addr->ai_protocol = 6;
-        addr->ai_canonname = name;
+        addr->ai_canonname =  strdup(node);
         addr->ai_next = NULL;
         sock->sin_family = AF_INET;
+        sock->sin_port = htons(atoi(service));
         sock->sin_addr.s_addr = inet_addr(ip.c_str());
         *res = addr;
         db->updateCount(hostname, ip);
+        //dump_addr(*res, service);
         return 0;
     }
 
